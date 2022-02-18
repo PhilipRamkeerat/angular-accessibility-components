f480e49e1c6819044da7b1bd6704afe20c849a46
Philip Ramkeerat <philip_ramkeerat@hotmail.com>
2022-02-18 16:04:20 -0400

    Implement keyboard managed directive and move focus

--- src/app/shared/directives/keyboard-manager/keyboard-manager.directive.ts
+++ src/app/shared/directives/keyboard-manager/keyboard-manager.directive.ts
@@ -13,18 +13,35 @@ export class KeyboardManagerDirective {
   public manageKeys(event: KeyboardEvent): void {
     switch (event.key) {
       case 'ArrowUp':
-        console.log(this.items);
-        console.log('up');
+        this.moveFocus(ArrowDirection.RIGHT).focus();
         break;
       case 'ArrowDown':
-        console.log('down');
+        this.moveFocus(ArrowDirection.LEFT).focus();
         break;
       case 'ArrowLeft':
-        console.log('left');
+        this.moveFocus(ArrowDirection.LEFT).focus();
         break;
       case 'ArrowRight':
-        console.log('right');
+        this.moveFocus(ArrowDirection.RIGHT).focus();
         break;
     }
   }
+
+  public moveFocus(direction: ArrowDirection): KeyboardManagedItemDirective {
+    const items = this.items.toArray();
+    const currentSelectedIndex = items.findIndex(item => item.isFocused());
+    const targetElementFocus = items[currentSelectedIndex + direction]; // +1 or -1 to move by keyboard
+    if (targetElementFocus) {
+      return targetElementFocus;
+    }
+
+    return direction === ArrowDirection.LEFT
+      ? items[items.length - 1]
+      : items[0];
+  }
+}
+
+enum ArrowDirection {
+  LEFT = -1,
+  RIGHT = 1
 }
